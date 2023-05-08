cefb85dbbd191fc0322deee9f8a788d4e0f797b9
aramya <22577625+thamugadi@users.noreply.github.com>
2023-05-08 05:52:26 +0100

    timer

--- src/emulator.c
+++ src/emulator.c
@@ -1,14 +1,32 @@
 #include <chip8.h>
 
+#include <pthread.h>
+#include <unistd.h>
+#include <time.h>
+
 #define memory context.memory
 #define pc context.pc
 
 #define to_interpret(x) ((memory[x] == 0x00EE) || (memory[x] >> 12 == 1) || (memory[x] >> 12 == 2) || (memory[x] >> 12) == 0xB) // RET, JP, CALL
 
+#define INTERVAL_MS 16 // Approximately 60Hz (1000ms / 60 = 16.6667ms)
+
 struct context_s context;
 
+void* incr()
+{
+	struct timespec interval = {0, INTERVAL_MS * 1000000L}; // ms to ns
+	while(1)
+	{
+		nanosleep(&interval, 0);
+		context.dt++;
+	}
+	return 0;
+}
 void emulate()
 {
+	pthread_t incr_thread;
+    	pthread_create(&incr_thread, NULL, incr, NULL);
         int n=0;
 
         if (to_interpret(pc))
