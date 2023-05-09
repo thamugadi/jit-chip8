7ed13d6150238ca33a1865396e9b6aa1b1af68d4
aramya <22577625+thamugadi@users.noreply.github.com>
2023-05-09 04:13:04 +0100

    fixed main

--- src/main.c
+++ src/main.c
@@ -1,6 +1,49 @@
 #include <chip8.h>
 #include <GL/glut.h>
 
+#define DRAW_POS(px,py) \
+        glVertex2i(px, py); \
+        glVertex2i(px+PIXELSIZE, py); \
+        glVertex2i(px+PIXELSIZE, py+PIXELSIZE); \
+        glVertex2i(px, py+PIXELSIZE);
+
+void display()
+{
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glColor3f(1.0, 1.0, 1.0);
+        glBegin(GL_QUADS);
+
+	// emulation:
+	emulate_basic_block();
+
+        if (context.dt)
+        {                                       
+                context.dt--;
+        }
+
+
+        for (int i = 0; i < WIDTH; i++)
+        {
+                for (int j = 0; j < HEIGHT; j++)
+                {
+                        if (context.gfx[i][j])
+                        {
+                                DRAW_POS(i, j);
+                        }
+                }
+        }
+        glEnd();
+
+        glutSwapBuffers();
+}
+
+void initGL()
+{
+        gluOrtho2D(0, WIDTH * PIXELSIZE, 0, HEIGHT * PIXELSIZE);
+        glMatrixMode(GL_MODELVIEW);
+}
+
 int main(int argc, char** argv)
 {
 	if (argc < 2)
@@ -52,7 +95,7 @@ int main(int argc, char** argv)
 	glutKeyboardFunc(keyboardDown);
     	glutKeyboardUpFunc(keyboardUp);
 
-	glutIdleFunc(emulate_basic_block);
+	glutIdleFunc(display);
 
 	glutMainLoop();
 }
