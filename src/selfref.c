ad67799cf068023de3b5c710ffb85e2fade677ff
aramya <22577625+thamugadi@users.noreply.github.com>
2023-09-06 02:04:55 +0100

    saving context

--- src/selfref.c
+++ src/selfref.c
@@ -2,4 +2,15 @@
 
 void mem_handler(uint8_t* addr)
 {
+	asm("push rax");
+	asm("push rbx");
+	asm("push rcx");
+	asm("push rdx");
+	asm("push r10");
+	printf("a");
+	asm("pop r10");
+	asm("pop rdx");
+	asm("pop rcx");
+	asm("pop rbx");
+	asm("pop rax");
 }
