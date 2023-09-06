56457ff23a40259ee46357d70182b7abffc80622
aramya <22577625+thamugadi@users.noreply.github.com>
2023-09-06 01:49:48 +0100

    useless comments

--- src/selfref.c
+++ src/selfref.c
@@ -1,16 +1,5 @@
 #include <chip8.h>
 
-/* instructions involving reading chip8 memory and not 7 bytes long:
- * 88 21 
- * 8a 98 (32LE)
- * 88 1c 01
- * 8a 1c 08
- * 88 98 (32LE)
- * 
-*/
-/* other instructions involving reading chip8 memory are all 7 bytes long.*/
-/* * used registers: rax, rbx, rcx, rdx, r10 */
-
 void mem_handler(uint8_t* addr)
 {
 }
